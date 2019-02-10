3b2afe39422476ecf286b3d63f7836edb6100c95
ItsRicmor <ricardojms1999@gmail.com>
2019-02-10 14:31:24 -0600

    Set DataBase

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb://root:124578369!Rjms#@cafe-shard-00-00-z9vpx.mongodb.net:27017,cafe-shard-00-01-z9vpx.mongodb.net:27017,cafe-shard-00-02-z9vpx.mongodb.net:27017/test?ssl=true&replicaSet=cafe-shard-0&authSource=admin&retryWrites=true'
+    urlDB = 'mongodb://root:124578369!Rjms#@cafe-shard-00-00-z9vpx.mongodb.net:27017,cafe-shard-00-01-z9vpx.mongodb.net:27017,cafe-shard-00-02-z9vpx.mongodb.net:27017/cafe?ssl=true&replicaSet=cafe-shard-0&authSource=admin&retryWrites=true'
 }
 
 process.env.URLDB = urlDB
