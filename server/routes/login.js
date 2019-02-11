6e7d9ea2ae1b8e677fc8160c37db4eb2e2e7ed5f
ItsRicmor <ricardojms1999@gmail.com>
2019-02-11 16:16:42 -0600

    Google Sign In

--- server/routes/login.js
+++ server/routes/login.js
@@ -54,7 +54,7 @@ async function verify(token) {
         idToken: token,
         audience: CLIENT_ID
     });
-    const { name:nombre, email, picture:img } = ticket.getPayload() 
+    const { name: nombre, email, picture: img } = ticket.getPayload()
     return {
         nombre,
         email,
@@ -64,11 +64,67 @@ async function verify(token) {
 }
 
 
-app.post('/google', (req, res) => {
+app.post('/google', async (req, res) => {
     const { body: { token } } = req
     const googleUser = await verify(token)
-    res.json({
-        token
+        .catch(err => {
+            return res.status(403).json({
+                ok: false,
+                err
+            })
+        })
+    Usuario.findOne({ email: googleUser.email }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            })
+        }
+        if(usuarioDB) {
+            if(!usuarioDB.google){
+                return res.status(400).json({
+                    ok: false,
+                    err: {
+                        message: 'Debe de usar su autenticación normal'
+                    }
+                })
+            } else {
+                let token = jwt.sign({
+                    usuario: usuarioDB
+                }, SEED, { expiresIn: CADUCIDAD_TOKEN })
+
+                return res.json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token
+                })
+            }
+        } else {
+            // Sí el usuario no existe en nuestra base de datos
+            let usuario = new Usuario()
+            usuario.nombre = googleUser.nombre
+            usuario.email = googleUser.email
+            usuario.img = googleUser.img
+            usuario.google = true
+            usuario.password = ':)'
+            usuario.save((err, usuarioDB) => {
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        err
+                    })
+                }
+                let token = jwt.sign({
+                    usuario: usuarioDB
+                }, SEED, { expiresIn: CADUCIDAD_TOKEN })
+
+                return res.json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token
+                })
+            })
+        }
     })
 })
 
